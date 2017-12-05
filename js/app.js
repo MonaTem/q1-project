a9db8e1cfee3d872aa7f09752ee515ab4ee60e32
Mona Temchin <monatemchin@gmail.com>
2017-12-05 15:46:25 -0500

    Refactor--functions and array of objs

--- js/app.js
+++ js/app.js
@@ -7,107 +7,102 @@
   */
 
 
-var monsterFave = localStorage.getItem("monster");
-monsterFave = stringToBoolean(monsterFave);
-
-
 if (ON_INDEX) {
-  localStorage.removeItem("firstTime");
-  localStorage.setItem('firstTime', true);
 
-  if (monsterFave) {
+  checkLocalStorage();
+
+}  else {
+
+     var monsterName = document.querySelector("card-title").textContent;
+
+     if (monsterName === MONSTERNAME) {
+
+       monsterName.addEventListener("click", function() {
+
+          checkLocalStorage();
+          removeLocalStorage();
+          monsterList = monsterFaves;
+          storeArray();
+
+
+       });
+
+     }
+}
+
+
+function checkLocalStorage() {
+  if(!localStorage.getItem('monsterInfo')) {
+     populateStorage();
+} else {
+    checkMonsterFaves();
+
+}
+}
+
+function populateStorage() {
+
+var monsterList = [];
+
+monsterList = document.getElementsByClassName('monsters');
+
+ for (i=0; i< monsterList.length; i++) {
+    let myObj = {};
+    myObj.name = document.querySelector("title");
+    myObj.fave = false;
+    monsterList.push(myObj);
+  }
+
+}
+
+function storeArray(monsterList) {
+
+localStorage.setItem("monsterInfo", JSON.stringify(monsterList));
+
+}
+
+function checkMonsterFaves() {
+
+var monsterFaves = JSON.parse(localStorage.getItem('monsterInfo'));
+
+for (i=0; i< monsterFaves.length; i++) {
+
+  if (monsterFaves[i].fave && ON_INDEX) {
+    favIconIndex(monsterFaves[i]);
+  }
+
+  if (!ON_INDEX) {
+
     let hidden = document.querySelector(".hide");
     hidden.classList.toggle("hide");
+    hidden.classList.toggle("unhide");
+    let visible = document.querySelector(".unhide");
+    visible.classList.toggle("unhide");
+    visible.classList.toggle("hide");
 
-  }
-}
-
-if (!ON_INDEX) {
-
-  monsterFave = localStorage.getItem("monster");
-  monsterFave = stringToBoolean(monsterFave);
-
-
-  var firstTime = localStorage.getItem("firstTime");
-  firstTime = stringToBoolean(firstTime);
-
-
-  if (firstTime) {
-
-    localStorage.setItem('firstTime', false);
-    firstTime = false;
-
-    if (monsterFave) {
-
-      let hidden = document.querySelector(".hide");
-      hidden.classList.toggle("hide");
-      hidden.classList.toggle("unhide");
-      let visible = document.querySelector(".unhide");
-      visible.classList.toggle("unhide");
-      visible.classList.toggle("hide");
-
-    }
-
-  }
-
-}
-
-var monsterList = document.getElementsByClassName('monsters');
-
-for (var i = 0; i < monsterList.length; i++) {
-
-  monsterList[i].addEventListener("click", function() {
-
-    if (monsterFave) {
-      monsterFaveToUnfav();
+    if (monsterFaves[i].fave) {
+        monsterFaves[i].fave = false;
     } else {
-      monsterUnfavToFave();
+        monsterFaves[i].fave = true;
     }
 
-  });
-
-}
-
-
-function stringToBoolean(myString) {
-
-  var myBoolean = false;
-
-  switch (myString) {
-    case "true":
-      myBoolean = true;
-      break;
-    case "false":
-      myBoolean = false;
-      break;
-    default:
-      myBoolean = false;
-      break;
   }
 
-  return myBoolean;
-}
-
-function monsterFaveToUnfav() {
-  localStorage.setItem('monster', false);
-  monsterFave = false;
-  let hidden = document.querySelector(".hide");
-  hidden.classList.toggle("hide");
-  hidden.classList.toggle("unhide");
-  let visible = document.querySelector(".unhide");
-  visible.classList.toggle("unhide");
-  visible.classList.toggle("hide");
+ }
 
 }
 
-function monsterUnfavToFave() {
-  localStorage.setItem('monster', true);
-  monsterFave = true;
-  let hidden = document.querySelector(".hide");
-  hidden.classList.toggle("hide");
-  hidden.classList.toggle("unhide");
-  let visible = document.querySelector(".unhide");
-  visible.classList.toggle("unhide");
-  visible.classList.toggle("hide");
 
+function favIconIndex(myObj) {
+
+ if (document.querySelector("title")  === myObj.name) {
+
+   let hidden = document.querySelector(".hide");
+   hidden.classList.toggle("hide");
+
+ }
+
+}
+function removeLocalStorage() {
+   localStorage.removeItem("monsterInfo");
 }
