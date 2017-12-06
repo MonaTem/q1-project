87bec8d07ebf1a3a6e7137d4b7db42a832ed3e5f
Mona Temchin <monatemchin@gmail.com>
2017-12-06 17:13:05 -0500

    ongoing work on favicon logic for multiple pages

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-/*  $(document).ready(function() {
+ /*  $(document).ready(function() {
 
     $(".button-collapse").sideNav();
 
@@ -13,65 +13,83 @@ if (ON_INDEX) {
 
 }  else {
 
-     var monsterName = document.querySelector("card-title").textContent;
+     var monsterName = document.querySelector(".card-title").textContent;
 
      if (monsterName === MONSTERNAME) {
 
-       monsterName.addEventListener("click", function() {
+      let btns = document.getElementsByClassName("monsters");
 
-          checkLocalStorage();
+      for (j=0; j<2; j++) {
+
+      btns[j].addEventListener("click", function() {
+
+          var monsterFavorites = checkLocalStorage();
           removeLocalStorage();
-          monsterList = monsterFaves;
-          storeArray();
-
+          var listOfMonsters = monsterFavorites;
+          storeArray(listOfMonsters);
 
        });
-
+      }
      }
 }
 
-
 function checkLocalStorage() {
   if(!localStorage.getItem('monsterInfo')) {
+    if (!ON_INDEX) {
      populateStorage();
+     }
 } else {
-    checkMonsterFaves();
+    var monsterFavs = [];
+    monsterFavs = checkMonsterFaves();
 
 }
+return monsterFavs;
 }
 
 function populateStorage() {
 
+var monsterArray = [];
+
+monsterArray = document.getElementsByClassName('card-title');
+
+var myObj = {};
 var monsterList = [];
 
-monsterList = document.getElementsByClassName('monsters');
-
- for (i=0; i< monsterList.length; i++) {
-    let myObj = {};
-    myObj.name = document.querySelector("title");
+ for (i=0; i < monsterArray.length; i++) {
+    myObj.name = document.querySelector("title").textContent;
     myObj.fave = false;
     monsterList.push(myObj);
   }
+ storeArray(monsterList);
 
 }
 
-function storeArray(monsterList) {
+function storeArray(array) {
 
-localStorage.setItem("monsterInfo", JSON.stringify(monsterList));
+localStorage.setItem("monsterInfo", JSON.stringify(array));
 
 }
 
 function checkMonsterFaves() {
 
-var monsterFaves = JSON.parse(localStorage.getItem('monsterInfo'));
+monsterFaves = JSON.parse(localStorage.getItem('monsterInfo'));
 
-for (i=0; i< monsterFaves.length; i++) {
+for (var i=0; i< monsterFaves.length; i++) {
 
   if (monsterFaves[i].fave && ON_INDEX) {
     favIconIndex(monsterFaves[i]);
   }
 
   if (!ON_INDEX) {
+
+    monsterFaves[i].fave = favesMonsterPages(monsterFaves[i].fave);
+
+  }
+}
+return monsterFaves;
+}
+
+function favesMonsterPages(fave)  {
 
     let hidden = document.querySelector(".hide");
     hidden.classList.toggle("hide");
@@ -80,22 +98,17 @@ for (i=0; i< monsterFaves.length; i++) {
     visible.classList.toggle("unhide");
     visible.classList.toggle("hide");
 
-    if (monsterFaves[i].fave) {
-        monsterFaves[i].fave = false;
+    if (fave) {
+        fave = false;
     } else {
-        monsterFaves[i].fave = true;
+        fave = true;
     }
-
+    return fave;
   }
 
- }
+function favIconIndex(Obj) {
 
-}
-
-
-function favIconIndex(myObj) {
-
- if (document.querySelector("title")  === myObj.name) {
+ if (document.querySelector("title")  === Obj.name) {
 
    let hidden = document.querySelector(".hide");
    hidden.classList.toggle("hide");
